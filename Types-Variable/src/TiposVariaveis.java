6fa7ed0e9550f0fc3521939c5a981a6005ab1ccf
DevNovaes <viniciusnovaes2@gmail.com>
2024-05-22 09:56:16 -0300

    aula tipos de variáveis

--- Types-Variable/src/TiposVariaveis.java
+++ Types-Variable/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salario = 2500; /* dois mil e quinhentos */
+        double salario2 = 2.500; /* vinte e cinquenta */
+        
+        int idade; //Tipo 'int'; nome 'idade'; nenehum valor foi atribuído
+        int ano; //Tipo 'int'; nome 'ano'; atribuição = 2021.
+        
+
+        /* peculiaridades */
+
+        byte idad = 123;
+
+        short years = 2021;
+
+        int cep = 21070333;  // é recomendado escolher outro tipo de variávrl, pois int não números começando com 0
+
+        long cpf = 98765432109L;  // variáveis 'long' devem conter 
     }
 }
